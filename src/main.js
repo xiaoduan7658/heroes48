f3939d15510d0835befcdcf4e97994b0a914afdc
段瑞统 <761690971@qq.com>
2018-11-21 11:04:05 +0800

    添加首页

--- src/main.js
+++ src/main.js
@@ -1,6 +1,16 @@
-import Vue from 'vue'
-import App from './App.vue'
 
+//导入模块 模块语法CommonJs
+// const Vue = require('vue');
+
+//ES6中模块化的语法
+import Vue from 'vue';
+import App from './App.vue';
+
+//导入全局模式
+//导入bootstrap
+import 'bootstrap/dist/css/bootstrap.css'
+//导入index.css
+import './assets/css/index.css'
 new Vue({
   el: '#app',
   render: h => h(App)
